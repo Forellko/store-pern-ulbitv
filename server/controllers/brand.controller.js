bb083fb6493089d35cf739b733a3c64d8048f440
forellko <dragonforce97q@gmail.com>
2022-12-11 01:15:08 +0300

    add functions to brand controller

--- server/controllers/brand.controller.js
+++ server/controllers/brand.controller.js
@@ -1,5 +1,14 @@
-const create = async (req, res) => {};
+const { Brand } = require('../models/models');
 
-const getAll = async (req, res) => {};
+const create = async (req, res) => {
+  const { name } = req.body;
+  const brand = await Brand.create({ name });
+  return res.json(brand);
+};
+
+const getAll = async (req, res) => {
+  const brands = await Brand.findAll();
+  return res.json(brands);
+};
 
 module.exports = { create, getAll };
