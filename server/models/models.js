813a92ce02b84ae76d9d19d4443dcba13e55a0d3
forellko <dragonforce97q@gmail.com>
2022-12-10 22:20:58 +0300

    add cors and express.json to express app

--- server/models/models.js
+++ server/models/models.js
@@ -2,7 +2,7 @@ const sequelize = require('../db');
 const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: 'User' },
