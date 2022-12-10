6d8281eca0f1df0851d52053b1ff115063249da3
forellko <dragonforce97q@gmail.com>
2022-12-10 23:16:12 +0300

    add error handler

---
+++ server/controllers/device.controller.js
@@ -0,0 +1,7 @@
+const create = async (req, res) => {};
+
+const get = async (req, res) => {};
+
+const getOne = (req, res) => {};
+
+module.exports = { create, get };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,15 @@
+const ApiError = require('../error/api.error');
+
 const registration = async (req, res) => {};
 
 const login = async (req, res) => {};
 
-const check = async (req, res) => {};
+const check = async (req, res, next) => {
+  const { id } = req.query;
+
+  if (!id) return next(ApiError.badRequest('id undefined'));
+
+  res.json(id);
+};
 
 module.exports = { registration, login, check };
