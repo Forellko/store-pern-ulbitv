372fed1f77ba78127c774cdc9351da403ece07aa
forellko <dragonforce97q@gmail.com>
2022-12-10 18:27:40 +0300

    add models core

--- server/models/models.js
+++ server/models/models.js
@@ -8,3 +8,43 @@ const User = sequelize.define('user', {
   role: { type: DataTypes.STRING, defaultValue: 'User' },
 });
 
+const Basket = sequelize.define('basket', {
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+});
+
+const BasketDevice = sequelize.define('basket_device', {
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+});
+
+const Device = sequelize.define('device', {
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
+
+const Type = sequelize.define('type', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Brand = sequelize.define('brand', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Rating = sequelize.define('rating', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false },
+});
+const DeviceInfo = sequelize.define('type', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+User.hasOne(Basket);
+
+Device.hasMany(DeviceInfo);
+DeviceInfo.belongsTo(Device);
