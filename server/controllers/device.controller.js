5911aa790887576263186c8c1d7277a6c886b52f
forellko <dragonforce97q@gmail.com>
2022-12-11 12:42:15 +0300

    add uuid

--- server/controllers/device.controller.js
+++ server/controllers/device.controller.js
@@ -1,6 +1,9 @@
+const uuid = require('uuid')
+
 const create = async (req, res) => {
   const { name, price, brandId, typeId, info } = req.body;
   const {img} = req.files
+  let fileName = 
 };
 
 const getAll = async (req, res) => {};
