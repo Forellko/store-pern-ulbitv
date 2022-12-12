8dc41e1061d13f5ecd4429fba133dfeaf112f19c
forellko <dragonforce97q@gmail.com>
2022-12-12 17:20:10 +0300

    add getOne functional

--- server/controllers/device.controller.js
+++ server/controllers/device.controller.js
@@ -2,6 +2,7 @@ const uuid = require('uuid');
 const path = require('path');
 const { Device, DeviceInfo } = require('../models/models');
 const ApiError = require('../error/api.error');
+const { accessSync } = require('fs');
 
 const create = async (req, res, next) => {
   try {
@@ -68,6 +69,16 @@ const getAll = async (req, res) => {
   return res.json(devices);
 };
 
-const getOne = (req, res) => {};
+const getOne = async (req, res) => {
+  const { id } = req.params;
+  const device = await Device.findOne({
+    where: {
+      id,
+    },
+    include: [{ model: DeviceInfo, as: 'info' }],
+  });
+
+  return res.json(device);
+};
 
 module.exports = { create, getAll, getOne };
