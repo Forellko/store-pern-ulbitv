1c458ad00fedb4953c14c86a38539996d4979507
forellko <dragonforce97q@gmail.com>
2022-12-15 06:08:38 +0300

    add routes.js with page components

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Auth() {
+export function Auth() {
   return (
     <div>Auth</div>
   )
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function DevicePage() {
+export function DevicePage() {
   return (
     <div>DevicePage</div>
   )
