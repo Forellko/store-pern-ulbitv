54d6fd1ecbe9826780c6a54857a436a4fb02a9a1
forellko <dragonforce97q@gmail.com>
2022-12-15 19:16:27 +0300

    add navigate to shop

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
 
 export function AppRouter() {
   const isAuth = false;
 
   return (
-    <Routes>
-      {isAuth &&
-        authRoutes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} exact />
-        ))}
+    <>
+      <Routes>
+        {isAuth &&
+          authRoutes.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} exact />
+          ))}
 
-      {!isAuth &&
-        publicRoutes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} exact />
-        ))}
-    </Routes>
+        {!isAuth &&
+          publicRoutes.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} exact />
+          ))}
+      </Routes>
+      <Navigate replace to={SHOP_ROUTE} />
+    </>
   );
 }
