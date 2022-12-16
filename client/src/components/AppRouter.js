659af388917d11659a900ddc4b1e91ada97f6930
forellko <dragonforce97q@gmail.com>
2022-12-16 06:00:08 +0300

    add context

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Context } from '..';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/consts';
 
 export function AppRouter() {
+  const { user } = useContext(Context);
+
+  console.log(user);
+
   const isAuth = false;
 
   return (
     <>
       <Routes>
-        {isAuth &&
+        {user.isAuth &&
           authRoutes.map(({ path, Component }) => (
             <Route key={path} path={path} element={<Component />} exact />
           ))}
