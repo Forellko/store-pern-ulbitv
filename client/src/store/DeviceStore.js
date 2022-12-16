a5905b068d81127885b917d7c26fbabddc7802ba
forellko <dragonforce97q@gmail.com>
2022-12-16 06:07:57 +0300

    add DeviceStore

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,53 @@
+import { makeAutoObservable } from 'mobx';
+
+export default class DeviceStore {
+  constructor() {
+    this._types = [
+      {
+        id: 1,
+        name: 'hello',
+        id: 2,
+        name: 'world',
+      },
+    ];
+    this._brands = [
+      {
+        id: 1,
+        name: 'hello',
+        id: 2,
+        name: 'world',
+      },
+    ];
+    this._devices = [
+      {
+        id: 1,
+        name: 'hello',
+        id: 2,
+        name: 'world',
+      },
+    ];
+    makeAutoObservable(this);
+  }
+
+  setTypes(types) {
+    this._types = types;
+  }
+  setBrands(brands) {
+    this._brands = brands;
+  }
+  setDevices(devices) {
+    this._devices = devices;
+  }
+
+  get types() {
+    return this._types;
+  }
+
+  get brands() {
+    return this._brands;
+  }
+
+  get devices() {
+    return this._devices;
+  }
+}
