6a1145654bb388d5ed6665e46a3df1f0a0c53f4a
forellko <dragonforce97q@gmail.com>
2022-12-16 21:26:07 +0300

    put navbar to app.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React, { useContext } from 'react';
 import { Context } from '..';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
-export default function Navbar() {
+export const NavBar = () => {
   const { user } = useContext(Context);
 
-  return <div>Navbar</div>;
-}
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
