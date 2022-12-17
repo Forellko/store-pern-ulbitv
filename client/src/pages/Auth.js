b3bf31f0b10e522109ca2dc2ca844e65b394791c
forellko <dragonforce97q@gmail.com>
2022-12-17 20:54:16 +0300

    add auth page core

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Button, Card, Container, Form, NavLink, Row } from 'react-bootstrap';
+import { REGISTRATION_ROUTE } from '../utils/consts';
 
 export function Auth() {
   return (
-    <div>Auth</div>
-  )
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className="p-5">
+        <h2 className="m-auto">Авторизация</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control placeholder="введите email" className="mt-4" />
+          <Form.Control placeholder="введите пароль" className="mt-4" />
+          <Row className="d-flex justify-content-between align-items-center mt-3 px-4">
+            <span style={{ width: '300px' }}>
+              Нет аккаунта?
+              <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+            </span>
+            <Button variant="outline-success" style={{ width: '100px' }}>
+              Войти
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
+  );
 }
