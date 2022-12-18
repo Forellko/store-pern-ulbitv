e7315e6816573f113e26a568608282b3467f116a
forellko <dragonforce97q@gmail.com>
2022-12-18 03:07:14 +0300

    add typebar core

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import TypeBar from '../components/TypeBar';
 
 export function Shop() {
   return (
     <Container>
       <Row>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <TypeBar />
+        </Col>
         <Col md={9}></Col>
       </Row>
     </Container>
