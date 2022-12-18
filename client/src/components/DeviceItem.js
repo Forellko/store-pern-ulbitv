48bc296f840e81edba7c7306865363c1cbe59efb
forellko <dragonforce97q@gmail.com>
2022-12-19 00:37:59 +0300

    add device item basics

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Card, Col, Image } from 'react-bootstrap';
+import star from '../assets/star.jpg';
 
 export const DeviceItem = ({ device }) => {
-  return <Col md={3}>DeviceItem</Col>;
+  return (
+    <Col md={3}>
+      <Card style={{ width: 150, cursor: 'pointer' }} border={'light'}>
+        <Image width={150} height={150} src={device.img} />
+        <div>
+          <div>Samsung</div>
+          <div>
+            <div>{device.rating}</div>
+            <Image src={star} />
+          </div>
+        </div>
+      </Card>
+    </Col>
+  );
 };
