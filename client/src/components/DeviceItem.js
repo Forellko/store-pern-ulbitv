56aa80e58139d64c6f4a5348d21cc3b2ecafa174
forellko <dragonforce97q@gmail.com>
2022-12-19 00:41:44 +0300

    add star icon

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Card, Col, Image } from 'react-bootstrap';
-import star from '../assets/star.jpg';
+import star from '../assets/star.png';
 
 export const DeviceItem = ({ device }) => {
   return (
     <Col md={3}>
       <Card style={{ width: 150, cursor: 'pointer' }} border={'light'}>
         <Image width={150} height={150} src={device.img} />
-        <div>
+        <div className="d-flex justify-content-between align-items-center">
           <div>Samsung</div>
           <div>
             <div>{device.rating}</div>
-            <Image src={star} />
+            <Image height={20} width={20} src={star} />
           </div>
         </div>
       </Card>
