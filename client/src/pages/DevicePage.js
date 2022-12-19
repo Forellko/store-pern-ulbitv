119460bae48c735ee39b032f610aa514f2edd274
forellko <dragonforce97q@gmail.com>
2022-12-19 16:41:42 +0300

    finish device page layout

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap';
 import star from '../assets/star.png';
 
 export function DevicePage() {
@@ -10,29 +10,66 @@ export function DevicePage() {
     rating: 5,
     img: 'none',
   };
+  const description = [
+    { id: 1, title: 'Оперативная память', description: '5 гб' },
+    { id: 2, title: 'Камера', description: '12 мп' },
+    { id: 3, title: 'Процессор', description: 'Пентиум 3' },
+    { id: 4, title: 'Кол-во ядер', description: '2' },
+    { id: 5, title: 'Аккумулятор', description: '4000' },
+  ];
 
   return (
     <Container className="mt-3">
-      <Col md={4}>
-        <Image width={300} height={300} src={device.img} />
-      </Col>
-      <Col md={4}>
-        <Row className="d-flex flex-column align-items-center">
-          <h2>{device.name}</h2>
-          <div
-            className="d-flex align-items-center justify-content-center"
+      <Row>
+        <Col md={4}>
+          <Image width={300} height={300} src={device.img} />
+        </Col>
+        <Col md={4}>
+          <Row className="d-flex flex-column align-items-center">
+            <h2>{device.name}</h2>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                background: `url(${star}) no-repeat center center`,
+                width: 240,
+                height: 240,
+                backgroundSize: 'cover',
+                fontSize: 64,
+              }}
+            >
+              {device.rating}
+            </div>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <Card
+            className="d-flex flex-column align-items-center justify-content-around"
             style={{
-              background: `url(${star}) no-repeat center center`,
-              width: 240,
-              height: 240,
-              backgroundSize: 'cover',
+              width: 300,
+              height: 300,
+              fontSize: 32,
+              border: '5px solid lightgrey',
             }}
           >
-            {device.rating}
-          </div>
-        </Row>
-      </Col>
-      <Col md={4}></Col>
+            <h3>От: {device.price} руб.</h3>
+            <Button variant="outline-dark">Добавить в корзину</Button>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="d-flex flex-column m-3">
+        <h1>Характеристики</h1>
+        {description.map((info, index) => (
+          <Row
+            key={info.id}
+            style={{
+              background: index % 2 === 0 ? 'lightgray' : 'transparent',
+              padding: 10,
+            }}
+          >
+            {info.title}:{info.description}
+          </Row>
+        ))}
+      </Row>
     </Container>
   );
 }
